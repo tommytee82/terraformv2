82e08d0f01dabb1178c9ed917de4792cff5a8485
thomashornLM <thomas.horn@landmark.co.uk>
2022-10-24 15:15:16 +0100

    added gittest

---
+++ _testing/gittest/modules/StorageAccount/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "3.4.3"
+    }
+  }
+}
+/*
+resource "random_string" "random" {
+  length = 6
+  special = false
+  upper = false
+}
+
+*/
+resource "azurerm_storage_account" "example" {
+  #name                    = "${lower(var.stgact_name)}${random_string.random.result}stvm"
+  name                     = "${lower(var.stgact_name)}stvm"
+  resource_group_name      = var.rg_name
+  location                 = var.location
+  account_tier             = var.account_tier
+  account_replication_type = var.account_replication_type
+}
